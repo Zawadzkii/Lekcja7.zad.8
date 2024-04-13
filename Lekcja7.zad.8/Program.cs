116da0a36c32dc9d0e0e89104c3c10c4203b1bd4
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 07:15:38 +0200

    Add project files.

---
+++ Lekcja7.zad.8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Pobieranie wyniku z matury z matematyki od użytkownika
+		Console.WriteLine("Podaj wynik z matury z matematyki:");
+		int wynikMatematyka = Convert.ToInt32(Console.ReadLine());
+
+		// Sprawdzanie, czy kandydat spełnia kryterium
+		if (wynikMatematyka > 70)
+		{
+			Console.WriteLine("Możesz ubiegać się o miejsce na studiach.");
+		}
+		else
+		{
+			Console.WriteLine("Niestety, Twój wynik z matematyki nie spełnia wymogów.");
+		}
+	}
+}
